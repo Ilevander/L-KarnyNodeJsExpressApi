cef7480ed83c05a8d7ac2c528eba8c2e162461d2
dell <dell@DESKTOP-4P8U8AR>
2024-02-24 15:55:44 +0100

    express, body-parser, cors , mogoose

---
+++ Readme.md
@@ -0,0 +1,5 @@
+- Express : to build Backend full stack with JavaScript
+- Body-Parser : is a dependency using to simplify parsing files to send to API 
+- CORS : To Build API , let two different technologies talk together  
+- Mongoose : is a MongoDB object modeling tool designed to work in an asynchronous environment. Mongoose supports Node.js 
+
---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+//Creating the app (express Application)
+const app = express();
+
+//Using dependencies from Node modules for Express
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+let notes= [];
+//Building the API
+//POST:
+app.post('/notes', (req,res) => {
+    const body = req.body;
+    console.log("BODY: ", body);
+    notes.push(body);
+    res.send(true);
+});
+
+
+const port = 3000;
+
+app.listen(port, ()=> {
+    console.log(`Server has started on port ${port}...`);
+});
